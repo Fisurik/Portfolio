cee8aa2f70a8b722dc34160f5d168b72ff336a90
Suren <surogmk@gmail.com>
2023-06-02 21:30:32 +0400

    Creating components and projects massive

---
+++ src/pages/Projects.js
@@ -0,0 +1,21 @@
+
+import Project from '../components/projects/Project'
+
+
+const Projects = () => {
+    return ( 
+        <>
+    <main class="section">
+        <div class="container">
+            <h2 class="title-1">Projects</h2>
+            <ul class="projects">
+                {}
+            <Project/>
+            </ul>
+        </div>
+    </main>
+    </>
+     );
+}
+ 
+export default Projects;
